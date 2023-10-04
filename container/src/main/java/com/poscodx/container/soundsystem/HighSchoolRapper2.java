df4d494d5998525fb0fe38a10e0c0e69b78438da
hyojining <102501038+hyojining@users.noreply.github.com>
2023-10-04 15:46:22 +0900

    container #4 (Explicit Java Config)

--- container/src/main/java/com/poscodx/container/soundsystem/HighSchoolRapper2.java
+++ container/src/main/java/com/poscodx/container/soundsystem/HighSchoolRapper2.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HighSchoolRapper2 implements CompactDisc {
-	
+	private String title = "붕붕";
+	private String artist = "김하온";
+
 	@Override
 	public String play() {
-		return "Playing 붕붕 by 김하온";
+		return "Playing " + title + " by " + artist;
 	}
 	
 }
