00c505dbb969873c7ae295ebf266a19498744891
hyojining <102501038+hyojining@users.noreply.github.com>
2023-10-05 09:50:28 +0900

    container - Explicit(Java Config) #1

--- container/src/main/java/com/poscodx/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/poscodx/container/videosystem/DVDPlayer.java
@@ -3,6 +3,9 @@ package com.poscodx.container.videosystem;
 public class DVDPlayer {
 	private DigitalVideoDisc dvd;
 	
+	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
 	public String play() {
 		return dvd.play();
 	}
